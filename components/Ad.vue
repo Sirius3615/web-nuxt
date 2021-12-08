6f3ca2972735d5c64eecc5d0218c5cad01929ddb
Sirius <ivanbegonja0@gmail.com>
2021-12-08 11:09:40 +0100

    ad

--- components/Ad.vue
+++ components/Ad.vue
@@ -38,7 +38,7 @@ export default {
       this.$nextTick(() => {
         try {
           // this is required for each ad slot (calling this once will only load 1 ad)
-          (adsbygoogle = window.adsbygoogle || []).push({})
+          (window.adsbygoogle = window.adsbygoogle || []).push({})
         } catch (error) {
           console.error(error)
         }
