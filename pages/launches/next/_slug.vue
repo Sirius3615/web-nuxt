9f520c8cf3eea3a3f37ef0878b3d01cf93b47de1
Sirius <ivanbegonja0@gmail.com>
2021-12-08 10:09:24 +0100

    typo

--- pages/launches/next/_slug.vue
+++ pages/launches/next/_slug.vue
@@ -223,16 +223,16 @@ export default {
       // Facebook 
       { name: 'og:site _name', content: 'BSN' }, 
       { name: 'og:title', content: this.launched.name + ' - BSN' }, 
-      { name: 'og:description', content:' Get all information abutb the lounch!' },
+      { name: 'og:description', content:' Get all information about the lounch!' },
       { name: 'og:image', content: this.launched.image },
       // Twitter
       { name: 'twitter:title', content: this.launched.name + ' - BSN' },
-      { name: 'twitter:description', content: ' Get all information abutb the lounch!' },
+      { name: 'twitter:description', content: ' Get all information about the lounch!' },
       { name: 'twitter:image', content: this.launched.image },
       { name: 'twitter:card', content: 'summary_large_image' }, 
       // SEO
       { name: 'title', content: this.launched.name + ' - BSN' },
-      { name: 'description', content: ' Get all information abutb the lounch!' },
+      { name: 'description', content: ' Get all information about the lounch!' },
       { name: 'keywords', content: 'rocket, launches, news, spaceflight, nasa, spacex, starship, astronomy, apod, liftoff, tv, space' },
       { name: 'author', content: 'BSN' },
       { name: 'robots', content: 'index, follow' },
