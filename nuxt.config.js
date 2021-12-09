db2ed8238487e1dbdac13d75b3159049d2f90c2b
Sirius <ivanbegonja0@gmail.com>
2021-12-09 20:55:08 +0100

    remove module

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,6 @@ export default {
     '@nuxtjs/google-analytics',
     '@nuxtjs/pwa',
     '@nuxtjs/sitemap',
-    '@nuxt/image',
     ['@nuxtjs/google-adsense', {
       id: 'ca-pub-4670880031318337'
     }],
@@ -20,7 +19,6 @@ export default {
 
   serverMiddleware: {
     '/api': '~/api/index.js',
-    '/_ipx': '~/server/middleware/ipx.js'
 
   },
 
@@ -46,28 +44,5 @@ export default {
   sitemap: {
     hostname: 'https://beyondspacenews.com/',
     gzip: true,
-  },
-  image: {
-    domains: [
-              'https://spaceflightnow.com/',
-              'https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/',
-              'https://spacenews.com/',
-              'https://www.nasa.gov/',
-              'https://blog.ulalaunch.com/',
-              'https://planetary.s3.amazonaws.com/',
-              'https://www.nasaspaceflight.com/',
-              'https://spacex.com/',
-              'https://space.com/',
-              'https://phys.org/',
-              'https://arstechnica.com/',
-              'https://www.blueorigin.com/',
-              'https://www.spaceflightinsider.com/',
-              'https://www.japantimes.co.jp/',
-              'https://www.theverge.com/',
-              'https://www.teslarati.com/',
-              'https://www.elonx.net/',
-              'https://www.virgingalactic.com/',
-              'https://www.esa.int/'
-            ]
   }
 }
