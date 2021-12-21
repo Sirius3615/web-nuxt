32679def87d5ffc0b7fa801644ab6d66abea521f
Sirius <ivanbegonja0@gmail.com>
2021-12-21 17:10:11 +0100

    warning fiexed

--- pages/launches/_slug.vue
+++ pages/launches/_slug.vue
@@ -207,7 +207,7 @@
 </div>
 
 <br>
-<div class=" w-full lg:rounded-lg grid place-items-center h-auto lg:rounded-lg">
+<div class=" w-full grid place-items-center h-auto lg:rounded-lg">
 <adsbygoogle page-url="https://beyondspacenews.com/"/>
 </div>
 <br>
