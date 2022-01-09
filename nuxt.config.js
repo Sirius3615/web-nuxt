a4716481554838beda066ea0012ca0f9f95e5f5b
Sirius <ivanbegonja0@gmail.com>
2022-01-09 22:08:48 +0100

    changes before the changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,51 +1,26 @@
 export default {
   target: 'static',
   ssr: false,
+  loading: {
+    color: '#4ade80',
+  },
   components: true,
 
   buildModules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/google-analytics',
-    '@nuxtjs/pwa',
     '@nuxtjs/axios',
     ['@nuxtjs/google-adsense', {
       id: 'ca-pub-4670880031318337'
     }],
-    
-    
-  ],
-
-  build: {
-  },
-
-  serverMiddleware: [
-    { path: "/api", handler: "~/api/index.js" },
-
-
   ],
 
   googleAnalytics: {
     id: '285402647'
   },
 
-  pwa: {
-    manifest: {
-      name: 'BeyondSpaceNews',
-      lang: 'en',
-      description: '',
-      theme_color: '#34D399',
-      start_url: '/',
-      short_name: 'BSN',
-      useWebmanifestExtension: false,
-      display: 'standalone'
-    },
-    meta: {
-      /* meta options */
-    }
-  },
-
   generate: {
     fallback: true
-  }
+  },
   
 }
